ef84f0a1847c8c12eb4424e989142159c856797d
Josque <joost@wagensveld.eu>
2016-04-20 14:46:01 +0200

    added relative paths

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -19,14 +19,14 @@ int main(int argc, char * argv[]) {
 	//ImageFactory::setImplementation(ImageFactory::STUDENT);
 
 
-	ImageIO::debugFolder = "D:\\Users\\Rolf\\Downloads\\FaceMinMin";
+	ImageIO::debugFolder = "..\\..\\..\\testsets\\Debug";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
 
 
-
+    //"D:\\GIT\\HU-Vision-1516-Base\\testsets\\Set A\\TestSet Images\\female-3.png"
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("D:\\Users\\Rolf\\Downloads\\TestA5.jpg", *input)) {
+	if (!ImageIO::loadImage("..\\..\\..\\testsets\\\Set A\\TestSet Images\\female-3.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
